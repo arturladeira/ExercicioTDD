33655a638acbd2c728dc99b9661c9fcc5f29a8e6
arturladeira <arturladeira@id.uff.br>
2021-03-02 20:19:30 -0300

    criada a classe pagamento e as classes de teste

--- src/boleto/Boleto.java
+++ src/boleto/Boleto.java
@@ -1,5 +1,29 @@
 package boleto;
+import java.util.Date;
 
 public class Boleto {
+	private int codBarras;
+	private Date dataBol;
+	private float valPago;
 
+	
+	public int getCodBarras() {
+		return codBarras;
+	}
+	public void setCodBarras(int codBarras) {
+		this.codBarras = codBarras;
+	}
+	public Date getDataBol() {
+		return dataBol;
+	}
+	public void setDataBol(Date dataBol) {
+		this.dataBol = dataBol;
+	}
+	public float getValPago() {
+		return valPago;
+	}
+	public void setValPago(float valPago) {
+		this.valPago = valPago;
+	}
+	
 }
--- src/fatura/Fatura.java
+++ src/fatura/Fatura.java
@@ -1,5 +1,31 @@
 package fatura;
 
+import java.util.Date;
+
 public class Fatura {
+	
+	private String Cliente;
+	private Date dataFat;
+	private float valTotal;
+	
+	public String getCliente() {
+		return Cliente;
+	}
+	public void setCliente(String cliente) {
+		Cliente = cliente;
+	}
+	public Date getDataFat() {
+		return dataFat;
+	}
+	public void setDataFat(Date dataFat) {
+		this.dataFat = dataFat;
+	}
+	public float getValTotal() {
+		return valTotal;
+	}
+	public void setValTotal(float valTotal) {
+		this.valTotal = valTotal;
+	}
+	
 
 }
