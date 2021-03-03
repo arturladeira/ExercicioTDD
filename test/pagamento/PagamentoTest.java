fcb6ab5c1fbd8de8e1c83c0ffa2b71e5bccb22f8
arturladeira <arturladeira@id.uff.br>
2021-03-03 10:31:56 -0300

    Teste de pagamento de fatura

--- test/pagamento/PagamentoTest.java
+++ test/pagamento/PagamentoTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.ArrayList;
+import java.util.Date;
 
 import boleto.Boleto;
 import fatura.Fatura;
@@ -15,7 +16,7 @@ public class PagamentoTest {
 	@Test
 	public void adicionarBoleto() {
 		Pagamento pag = new Pagamento();
-		Boleto boleto1 = new Boleto(123, new java.util.Date(01-02-2020), 200);
+		Boleto boleto1 = new Boleto(123, new Date(), 200);
 		pag.addBoleto(boleto1);
 		assertTrue(pag.getQtdeItems() == 1);
 	}
@@ -24,7 +25,7 @@ public class PagamentoTest {
 	public void pagarUmBoleto() {
 		Pagamento pag = new Pagamento();
 		double valorPago = 0;
-		Boleto boleto1 = new Boleto(123, new java.util.Date(01-02-2020), 200);
+		Boleto boleto1 = new Boleto(123, new Date(), 200);
 		pag.addBoleto(boleto1);
 		valorPago = pag.pagarBoleto();
 		Assertions.assertEquals(valorPago, boleto1.getValPago());
@@ -34,12 +35,23 @@ public class PagamentoTest {
 	public void pagarDoisBoletos() {
 		Pagamento pag = new Pagamento();
 		double valorPago = 0;
-		Boleto boleto1 = new Boleto(123, new java.util.Date(01-02-2020), 200);
-		Boleto boleto2 = new Boleto(1234, new java.util.Date(02-02-2020), 300);
+		Boleto boleto1 = new Boleto(123, new Date(), 200);
+		Boleto boleto2 = new Boleto(1234, new Date(), 300);
 		pag.addBoleto(boleto1);
 		pag.addBoleto(boleto2);
 		valorPago = pag.pagarBoleto();
 		Assertions.assertEquals(500, valorPago);
+	}
+	
+	@Test
+	public void pagarFatura() {
+		Pagamento pag = new Pagamento();
+		Fatura fat = new Fatura("Artur", new Date(), 600);
+		double valorPago = 0;
+		pag.addBoleto(new Boleto(123, new Date(), 200));
+		pag.addBoleto(new Boleto(1234, new Date(), 300));
+		valorPago = pag.pagarBoleto();
+		Assertions.assertEquals(fat.getValTotal(), valorPago);
 	}
 
 }
