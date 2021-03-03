04f0c9792377ccdf278d8b7201686d0fb15bc725
arturladeira <arturladeira@id.uff.br>
2021-03-02 23:03:01 -0300

    reteste pagar um boleto

--- test/pagamento/PagamentoTest.java
+++ test/pagamento/PagamentoTest.java
@@ -15,28 +15,28 @@ public class PagamentoTest {
 	@Test
 	public void adicionarBoleto() {
 		Pagamento pag = new Pagamento();
-		
 		Boleto boleto1 = new Boleto(123, new java.util.Date(01-02-2020), 200);
 		pag.addBoleto(boleto1);
 		assertTrue(pag.getQtdeItems() == 1);
 	}
 	
 	@Test
-	public void pagarBoleto() {
+	public void pagarUmBoleto() {
 		Pagamento pag = new Pagamento();
-		float valorPago = 0;
+		double valorPago = 0;
 		Boleto boleto1 = new Boleto(123, new java.util.Date(01-02-2020), 200);
-		pag.pagarBoleto();
+		pag.addBoleto(boleto1);
+		valorPago = pag.pagarBoleto();
 		Assertions.assertEquals(valorPago, boleto1.getValPago());
 	}
 	
 	@Test
 	public void pagarDoisBoletos() {
 		Pagamento pag = new Pagamento();
-		float valorPago = 0;
+		double valorPago = 0;
 		Boleto boleto1 = new Boleto(123, new java.util.Date(01-02-2020), 200);
 		Boleto boleto2 = new Boleto(1234, new java.util.Date(02-02-2020), 300);
-		pag.pagarBoleto();
+		valorPago = pag.pagarBoleto();
 		Assertions.assertEquals(500, valorPago);
 	}
 
