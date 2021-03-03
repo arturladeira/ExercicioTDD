48de50c97eb9b97e51d72624adc4a09ef182a832
arturladeira <arturladeira@id.uff.br>
2021-03-03 11:17:57 -0300

    Refatoração dos testes

--- test/pagamento/PagamentoTest.java
+++ test/pagamento/PagamentoTest.java
@@ -24,22 +24,26 @@ public class PagamentoTest {
 		boletoList = new ArrayList<Boleto>();
 	}
 	
+	@DisplayName("Adiciona um boleto na lista de pagamento")
 	@Test
 	public void adicionarBoleto() {
-		
-		Boleto boleto1 = new Boleto(123, new Date(), 200);
-		pag.addBoleto(boleto1);
+		boletoList.add(new Boleto(123, new Date(), 200));
+		pag.addBoleto(boletoList);
 		assertTrue(pag.getQtdeItems() == 1);
 	}
 	
+	@DisplayName("Pagamento de um boleto")
 	@Test
 	public void pagarUmBoleto() {
-		
 		double valorPago = 0;
-		Boleto boleto1 = new Boleto(123, new Date(), 200);
-		pag.addBoleto(boleto1);
-		valorPago = pag.pagarBoleto();
-		Assertions.assertEquals(valorPago, boleto1.getValPago());
+		double valorTotalBoleto = 0;
+		boletoList.add(new Boleto(123, new Date(), 200));
+		valorPago = pag.pagarBoleto(boletoList);
+		pag.addBoleto(boletoList);
+		for (Boleto boleto : boletoList) {
+			valorTotalBoleto += boleto.getValPago();
+		}
+		Assertions.assertEquals(valorPago, valorTotalBoleto);
 	}
 	
 	@DisplayName("Testa o somatório dos valores dos boleto e o pagamento dos boletos")
@@ -58,14 +62,14 @@ public class PagamentoTest {
 		Assertions.assertEquals(valorTotalBoleto, valorPago);
 	}
 	
+	@DisplayName("Testa o status da fatura para pagamento menores de fatura")
 	@Test
 	public void pagarFatura() {
-		
 		Fatura fat = new Fatura("Artur", new Date(), 600);
 		double valorPago = 0;
-		pag.addBoleto(new Boleto(123, new Date(), 200));
-		pag.addBoleto(new Boleto(1234, new Date(), 300));
-		valorPago = pag.pagarBoleto();
+		boletoList.add(new Boleto(123, new Date(), 200));
+		boletoList.add(new Boleto(1234, new Date(), 300));
+		valorPago = pag.pagarBoleto(boletoList);
 		Assertions.assertEquals(pag.getStatus(valorPago, fat.getValTotal()), "ABERTO");
 	}
 
