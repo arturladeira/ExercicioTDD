1d18b6e7cb926327735b88d82df0acb4c24e2f67
arturladeira <arturladeira@id.uff.br>
2021-03-02 22:04:28 -0300

    Teste fatura

--- test/fatura/FaturaTest.java
+++ test/fatura/FaturaTest.java
@@ -1,6 +1,19 @@
 package fatura;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import fatura.Fatura;
+
 public class FaturaTest {
+	
+	
+	@Test
+	public void verificaValorTotal() {
+		Fatura bol = new Fatura();
+		Assertions.assertEquals(200, bol.getValTotal());
+	}
+	
 	
 	
 
