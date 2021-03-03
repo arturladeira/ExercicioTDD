b02d1fe3ea4d2070d2628e1a31efce5f8a40f7e9
arturladeira <arturladeira@id.uff.br>
2021-03-02 22:28:50 -0300

    pagar dois boletos

--- test/pagamento/PagamentoTest.java
+++ test/pagamento/PagamentoTest.java
@@ -16,5 +16,16 @@ public class PagamentoTest {
 		valorPago = pag.pagarBoleto(boleto1);
 		Assertions.assertEquals(valorPago, boleto1.getValPago());
 	}
+	
+	@Test
+	public void pagarDoisBoletos() {
+		Pagamento pag = new Pagamento();
+		float valorPago = 0;
+		Boleto boleto1 = new Boleto(123, new java.util.Date(01-02-2020), 200);
+		Boleto boleto2 = new Boleto(1234, new java.util.Date(02-02-2020), 300);
+		valorPago = pag.pagarBoleto(boleto1);
+		valorPago = pag.pagarBoleto(boleto2);
+		Assertions.assertEquals(500, valorPago);
+	}
 
 }
